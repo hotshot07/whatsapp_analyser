c98333a429f0b233733e3f73be7b9c162cf6e64b
hotshot07 <aroram@tcd.ie>
2020-07-03 03:52:39 +0100

    perfected chat activity

--- chat_activity.py
+++ chat_activity.py
@@ -5,65 +5,45 @@ import seaborn as sns
 import warnings
 warnings.filterwarnings("ignore")
 import datetime
-
+import random
 from datetime import date
 
 df = pd.read_csv("userdata/chat.csv")
 
 df_day = dict(df["Date"].dropna().value_counts(sort=False))
 
-# print(df_day)
-
 date_range = pd.date_range(start=str(df["Date"][0]), end=str(df["Date"][df.index[-1]]))
 
 chat_list = []
 
+manage_labels = []
+
 for i in date_range:
     if str(i.date()) in df_day.keys():
         chat_list.append(df_day[str(i.date())])
+        manage_labels.append((i.date()))
     else:
         chat_list.append(0)
+        manage_labels.append("")
 
 final_data = date_range.to_frame(name="Date", index=False)
 
 final_data["Messages"] = chat_list
 
-plt.figure(figsize=(20, 10))
+final_labels = []
 
-#chart = sns.lineplot(final_data["Date"], final_data["Messages"])
-
-#chart.set_xticklabels(chart.get_xticklabels(), rotation=90)
-
-# df['week_number_of_year'] = datetime.date(df['Date'][0]).dt.week
-
-# week_number_of_year = []
-
-# for i in range(len(df["Date"])):
-#     try:
-#         date_obj = datetime.date.fromisoformat(df['Date'][i])
-#         string_obj = str(date_obj.isocalendar()[0]) + '_' + str(date_obj.isocalendar()[1])
-#         week_number_of_year.append(string_obj)
-#     except:
-#         week_number_of_year.append(" ")
+for x in manage_labels:
+    if type(x) is datetime.date and random.random() < 0.3:
+        final_labels.append(x.strftime("%B %d, %Y"))
+    else:
+        final_labels.append('')
 
 
-# df["Year_week"] = week_number_of_year
+plt.figure(figsize=(25, 10))
 
-# df_day2 = df["Year_week"].value_counts()
 
-# df_day = sorted(dict(df_day2))
-# df_day.remove(' ')
+chart = sns.barplot(final_data["Date"], final_data["Messages"], alpha=1, palette='rocket', data=final_data)
 
-# df_day3 = {}
-# for i in df_day:
-#     df_day3[str(i)] = df_day2[i]
-
-# print(df_day3)
-sns.scatterplot(final_data["Date"], final_data["Messages"], alpha=0.8, palette='rocket', data=final_data)
-
+chart.set_xticklabels(final_labels, rotation=90, fontweight='light', horizontalalignment='center', fontsize=6)
+plt.tight_layout()
 plt.show()
-
-# date_obj = datetime.datetime.strptime(df['Date'][i], '%Y-%m-%d')
-#         print(date_obj)
-#         print(date_obj.date().isoformat().isocalendar())
-#         print()
