176546f02731a56468ee583d776a5de5d10db802
hotshot07 <aroram@tcd.ie>
2020-07-06 14:17:29 +0100

    removed use of ntlk stopwords

--- wc.py
+++ wc.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from wordcloud import WordCloud
-from nltk.corpus import stopwords
+#from nltk.corpus import stopwords
 import matplotlib.pyplot as plt
 import re
 
@@ -27,17 +27,34 @@ def make_wordcloud(p_num):
             word = word.lower().strip()
             tokenized_list.append(word)
 
-    #add a word to this list to make it not appear in the wordcloud
+    # add a word to this list to make it not appear in the wordcloud
     custom_list = ['omitted', 'image', "<media", "omitted>", 'hai', "hai", 'nan', 'well', 'spam', 'https', 'Mayank', 'mayank', 'image', 'video', 'message', 'deleted', 'pdf', 'gif', 'deleted',
                    'sticker', "i’ll", "i’m", "Messages", "to", "this", "group", "are", "now", "secured", "with", "to", "end", "end encryption"]
 
-    #adding them hindi stopwords
-    all_stopwords = list(stopwords.words('english'))
+    english_stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he',
+                         'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it',
+                         "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what',
+                         'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are',
+                         'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did',
+                         'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of',
+                         'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before',
+                         'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under',
+                         'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where',
+                         'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other',
+                         'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too',
+                         'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now',
+                         'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't",
+                         'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven',
+                         "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn',
+                         "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't",
+                         'won', "won't", 'wouldn', "wouldn't"]
+
+    # adding them hindi stopwords
     hindi_stoplist = open("hindi_stoplist.txt").readlines()
     hindi_stoplist = [i.replace('\n', '') for i in hindi_stoplist]
 
-    #actually a hinglish stoplist
-    all_stopwords = hindi_stoplist + custom_list
+    # actually a hinglish stoplist
+    all_stopwords = english_stopwords + hindi_stoplist + custom_list
 
     without_stopwords = []
 
@@ -51,8 +68,8 @@ def make_wordcloud(p_num):
 
     links = []
 
-    # saving links for some reason in another list
-    # making wordcloud without links as it looks very weird 
+    # saving links for some reason in another list (next feature maybe?)
+    # making wordcloud without links as it looks very weird
     for data in without_stopwords:
         link = re.match(all_links, data)
         if link:
@@ -74,3 +91,8 @@ def make_wordcloud(p_num):
     plt.tight_layout(pad=0)
     plt.savefig("output/wordcloud.png")
     return
+
+
+# Testing
+if __name__ == '__main__':
+    make_wordcloud(2)
